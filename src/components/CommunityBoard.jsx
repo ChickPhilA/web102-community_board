731af354afe05894dc382291f438d4c7829fa1e2
Phil <natsudragoneel282003@gmail.com>
2026-06-05 17:39:32 -0700

    Only added Naruto's card so far for the community board; used it as a test to see the result of the card design

--- src/components/CommunityBoard.jsx
+++ src/components/CommunityBoard.jsx
@@ -1,15 +1,17 @@
+import Card from './Card.jsx'
 
 const CommunityBoard = () => {
     return (
         /* We want to put a grid here, two rows, five columns, of all of Konoka's shinobi. */
         <div className="container">
-            <div className="card">
-
-            </div>
-
-            <div className="card">
-                
-            </div>
+            <Card name = 'Naruto Uzumaki'
+            image="https://i.redd.it/ldsg25lcy9781.jpg"
+            alt="Naruto Uzumaki as a teenager"
+            description="The Jinchuuruki of the Nine Tails and the Hero of the Hidden Leaf.
+            7th Hokage of Konohagakure."
+            color="#8b0000"
+            link="https://naruto.fandom.com/wiki/Naruto_Uzumaki"
+            />
         </div>
     )
 }
