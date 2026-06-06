3070c83a166cb8abea9a5ca632ebb0af7356b8d9
Phil <natsudragoneel282003@gmail.com>
2026-06-05 17:39:02 -0700

    Created a structural layout of how the card should be with HTML elements as well as the character props being passed from the Community Board component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,27 @@
+/* The character card will pass in props such as character picture, name, description, and Narutopedia link. */
+
+/*
+Character object props:
+-name
+-image
+-alt (alt description for image)
+-description
+-color theme
+
+*/
+
+
 const Card = (character) => {
     return(
-        <div>
+        <div className="card">
+            <img src={character.image} alt={character.alt}></img>
+            <h3 className="card-name" style={{backgroundColor: character.color}}> {character.name} </h3>
+            <div className="card-info">
+                <p className="card-description"> {character.description} </p>
+                <a href={character.link} target="_blank"> Learn more about {character.name}!</a>
+            </div>
         </div>
     )
 }
+
+export default Card;
