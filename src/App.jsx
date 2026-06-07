b76405a608db84d6aacaa0ae41e73cbd5d86e804
Phil <natsudragoneel282003@gmail.com>
2026-06-06 21:09:20 -0700

    Created a state and refs for the BGM audio button to play

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-// import { useState } from 'react'
-// gonna comment useState knowing we don't have to use this for now but significant in future React concepts
+import { useState, useRef } from 'react'
 
 import './App.css'
 import CommunityBoard from './components/CommunityBoard.jsx'
 
 const App = () => {
+
+  const audioRef = useRef(null) // will connect to the <audio> element
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  // custom audio toggle function to get background music playing
+  const toggleAudio = () => {
+    if(isPlaying) {
+      audioRef.current.pause()
+    } else {
+      audioRef.current.play()
+    }
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className="app">
@@ -12,9 +24,13 @@ const App = () => {
       <h2> Get to learn more about your favorite shinobi that help maintain peace in the ninja world. </h2>
       <CommunityBoard />
 
-      <audio autoPlay loop controls>
+      <audio ref={audioRef} loop>
         <source src="/silhouette.mp3" type="audio/mpeg" />
       </audio>
+
+      <button onClick = {toggleAudio} className="bgm-button">
+        {isPlaying ? "Pause BGM 🔇" : "Play BGM 🔊"}
+      </button>
     </div>
   )
 }
