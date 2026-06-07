413e9efe5c1db64be06958d0dd212c25b511375a
Phil <natsudragoneel282003@gmail.com>
2026-06-06 20:16:28 -0700

    Added team prop to card of each character

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,9 @@ Character object props:
 -name
 -image
 -alt (alt description for image)
+-team name
 -description
 -color theme
-
 */
 
 
@@ -16,6 +16,7 @@ const Card = (character) => {
         <div className="card">
             <img src={character.image} alt={character.alt}></img>
             <h3 className="card-name" style={{backgroundColor: character.color}}> {character.name} </h3>
+            <p className="card-team">{character.team}</p>
             <div className="card-info">
                 <p className="card-description"> {character.description} </p>
                 <a href={character.link} target="_blank"> Learn more about {character.name}!</a>
